906f0f85c40a5b419f26f22625a6ac65a945bd1c
AishuSenthil <124016737+AishuSenthil@users.noreply.github.com>
2024-06-08 09:17:07 -0500

    GA4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 --Part 1
+--id int
+--employer VARCHAR(255)
+--name VARCHAR(255)
+--skills VARCHAR(255)
 
 --Part 2
+SELECT name FROM employer
+WHERE location = "St. Louis City";
 
 --Part 3
+DROP TABLE Job;
 
 --Part 4
+  SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
